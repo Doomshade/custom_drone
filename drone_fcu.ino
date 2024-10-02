a52a2c8cd627dbb4311384eb0ff1db90120d1ee6
Doomshade <kuba.smrha@gmail.com>
2024-10-02 20:48:50 +0200

    Possibly fix nullptr things

--- drone_fcu.ino
+++ drone_fcu.ino
@@ -6,10 +6,10 @@
 #include "recvr.h"
 #include "fcu.h"
 
-static mpu_t mpu;
-static esc_t esc;
-static recvr_t recvr;
-static fcu_t fcu;
+static mpu_t mpu = {};
+static esc_t esc = {};
+static recvr_t recvr = {};
+static fcu_t fcu = {};
 
 static inline void handle_command(cmd_t cmd) {
   switch (cmd.cmd) {
